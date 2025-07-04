73c61523bb52bab46d59e25d4b534cdf2d164a07
Sefik ZIROGLU <sefikziroglu@outlook.com>
2025-07-04 15:18:45 +0200

    4.2 blog list file structure

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,37 +1,31 @@
-require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 const app = express()
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
+logger.info('connecting to', config.MONGODB_URI)
 
-const Blog = mongoose.model('Blog', blogSchema)
+mongoose
+	.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info('connected to MongoDB')
+	})
+	.catch((error) => {
+		logger.error('error connection to MongoDB:', error.message)
+	})
 
-mongoose.connect(process.env.MONGODB_URI)
 
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
