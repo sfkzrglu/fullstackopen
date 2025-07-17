db07263ffcc73e50ac47cf5c4c56cc384c3cc01b
Sefik ZIROGLU <sefikziroglu@outlook.com>
2025-07-17 17:33:51 +0200

    4.6

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
 	return 1
 }
@@ -11,19 +13,33 @@ const totalLikes = (blogPosts) => {
 }
 
 const favoriteBlog = (blogs) => {
-	let max=0;
-	let blog=null;
-	blogs.forEach(_blog=>{
-		if (_blog.likes>max) {
-			max=_blog.likes
-			blog=_blog
+	let max = 0;
+	let blog = null;
+	blogs.forEach(_blog => {
+		if (_blog.likes > max) {
+			max = _blog.likes
+			blog = _blog
 		}
 	})
 	return blog
 }
 
+
+const mostBlogs = (blogs) => {
+	//count authors blogs
+	const count = _.countBy(blogs, 'author')
+	//find the author with max blog count
+	const maxAuthor = _.maxBy(_.toPairs(count,p=>p[1]))
+	return {
+		'author': maxAuthor[0],
+		'blogs':  maxAuthor[1]
+	}
+}
+
+
 module.exports = {
 	dummy,
 	totalLikes,
-	favoriteBlog
+	favoriteBlog,
+	mostBlogs
 }
