88f56b05b6bfb0d2a73e0a7229293a5743bee21f
Sefik ZIROGLU <sefikziroglu@outlook.com>
2025-08-16 15:03:20 +0300

    4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -29,17 +29,33 @@ const mostBlogs = (blogs) => {
 	//count authors blogs
 	const count = _.countBy(blogs, 'author')
 	//find the author with max blog count
-	const maxAuthor = _.maxBy(_.toPairs(count,p=>p[1]))
+	const maxAuthor = _.maxBy(_.toPairs(count, p => p[1]))
 	return {
 		'author': maxAuthor[0],
-		'blogs':  maxAuthor[1]
+		'blogs': maxAuthor[1]
 	}
 }
 
+
+const mostLikes = (blogs) => {
+	//group authors
+	const group = _.groupBy(blogs, "author")
+	const totalLikes = []
+	_.map(group, (item) => {
+		let likes = 0
+		item.forEach(k => likes += k.likes)
+		totalLikes.push({
+			"author": item[0].author,
+			"likes": likes
+		})
+	})
+	return _.maxBy(totalLikes,"likes")
+}
 
 module.exports = {
 	dummy,
 	totalLikes,
 	favoriteBlog,
-	mostBlogs
+	mostBlogs,
+	mostLikes
 }
