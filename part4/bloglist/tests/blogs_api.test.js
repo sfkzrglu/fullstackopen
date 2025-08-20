a082ad8d63f852fe5427c47b3d1ef7c2853e2d76
Sefik ZIROGLU <sefikziroglu@outlook.com>
2025-08-20 16:34:20 +0300

    4.10

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -4,13 +4,13 @@ const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const Blog = require("../models/blog");
-const helper = require("./test_helper");
+const helpers = require("./test_helper");
 
 const api = supertest(app);
 
 beforeEach(async () => {
 	await Blog.deleteMany({});
-	await Blog.insertMany(helper.initialBlogs);
+	await Blog.insertMany(helpers.initialBlogs);
 });
 
 
@@ -24,13 +24,35 @@ test("blogs are returned as JSON", async () => {
 test("all blogs are returned", async () => {
 	const response = await api.get("/api/blogs");
 
-	assert.strictEqual(response.body.length, helper.initialBlogs.length);
+	assert.strictEqual(response.body.length, helpers.initialBlogs.length);
 });
 
-
-test.only("unique identifier property of the blog posts is named id", async () => {
+test("unique identifier property of the blog posts is named id", async () => {
 	const response = await api.get("/api/blogs");
 	const hasUnderlineId = response.body.some(blog => blog._id)
 	assert.equal(hasUnderlineId, false);
 });
 
+test("a blog is created", async () => {
+	const newBlog = {
+		title: 'test post blog',
+		author: "Michael Chan",
+		url: "https://reactpatterns.com/",
+		likes: 10,
+	}
+	await api.post("/api/blogs")
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/);
+
+	const blogsInDb=await helpers.blogsInDb()
+	assert.strictEqual(blogsInDb.length,helpers.initialBlogs.length+1)
+
+	const titles=blogsInDb.map(t=>t.title)
+
+	assert(titles.includes('test post blog'))
+});
+
+after(async () => {
+  await mongoose.connection.close()
+})
